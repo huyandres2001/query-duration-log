32d2078a3c737ffff0f0cd8156516758a3bb15fa
huyandres <huyandres2001@gmail.com>
2025-04-03 20:36:42 +0700

    add http request logging

--- src/main/java/org/example/querydurationlogging/config/CustomStatisticsFactory.java
+++ src/main/java/org/example/querydurationlogging/config/CustomStatisticsFactory.java
@@ -5,6 +5,7 @@ import org.hibernate.stat.spi.StatisticsFactory;
 import org.hibernate.stat.spi.StatisticsImplementor;
 import org.springframework.beans.factory.annotation.Autowired;
 
+@SuppressWarnings("unused")
 public class CustomStatisticsFactory implements StatisticsFactory {
     @Override
     public StatisticsImplementor buildStatistics(@Autowired SessionFactoryImplementor sessionFactory) {
